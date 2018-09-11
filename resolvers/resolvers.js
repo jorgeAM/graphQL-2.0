321ad0b3db19499df9b8c8c0ce5d42889219aadd
Jorge L <jorge.alfmur@gmail.com>
2018-09-10 22:00:47 -0500

    authentication pt1

--- resolvers/resolvers.js
+++ resolvers/resolvers.js
@@ -1,7 +1,7 @@
 const User = require('../models/user');
 const Post = require('../models/post');
 const bcrypt = require('bcrypt');
-const jwt = require('../services/jwt');
+const authService = require('../services/jwt');
 
 const resolvers = {
   Mutation: {
@@ -11,6 +11,15 @@ const resolvers = {
       email: args.user.email,
       password:  await bcrypt.hash(args.user.password, 10),
     }),
+    login: async (_, args) => {
+      const user = await User.findOne({ email: args.credentials.email });
+      if (!user) throw new Error('Usuario NO encontrado');
+      const valid = await bcrypt.compare(args.credentials.password, user.password);
+      if (!valid) throw new Error('Contraseña incorrecta');
+      const token = authService.encode(user);
+      return { token, user };
+    },
+
     addPost: async (_, args) => await Post.create(args.post),
   },
   Query: {
